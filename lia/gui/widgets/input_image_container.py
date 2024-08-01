302f70d9d2748fdb476094e3539e6d02c56b1a61
knterada5 <knterada5@gmail.com>
2024-08-01 18:53:25 +0900

    Update docstring.

--- lia/gui/widgets/input_image_container.py
+++ lia/gui/widgets/input_image_container.py
@@ -1,15 +1,16 @@
 import flet as ft
 
-from lia.data.color_fvfm import ColorFvFmData
-
 
 class InputImage(ft.Column):
+    """Input image container with file path and select button."""
+
     def __init__(self, input_func, **kwargs):
         super().__init__(kwargs)
         self.input_func = input_func
-        self.set_content()
+        self.set_controls()
 
-    def set_content(self):
+    def set_controls(self):
+        """Set Controls."""
         self.text = ft.Text("Input image path.", expand=1, no_wrap=True)
         self.img = ft.Image(src="src/photo.png", expand=1)
         self.expand = True
@@ -22,7 +23,7 @@ class InputImage(ft.Column):
                         ft.TextButton(
                             "Select Image",
                             icon=ft.icons.INSERT_PHOTO_OUTLINED,
-                            on_click=self.select_image,
+                            on_click=self.click_select_button,
                         ),
                     ],
                     expand=1,
@@ -36,13 +37,21 @@ class InputImage(ft.Column):
             ),
         ]
 
-    def select_image(self, e):
+    def click_select_button(self, e):
+        """Fires when select button clicked."""
         file_picker = ft.FilePicker(on_result=self.input_filepicker)
         self.page.overlay.append(file_picker)
         self.page.update()
         file_picker.pick_files()
 
     def input_filepicker(self, e: ft.FilePickerResultEvent):
+        """Input from filepicker.
+
+        Parameters
+        ----------
+        e : ft.FilePickerResultEvent
+            FilePicker result event.
+        """
         if e.files is None:
             return
         input_path = e.files[0].path
@@ -51,6 +60,13 @@ class InputImage(ft.Column):
         self.page.update()
 
     def set_image(self, input_path):
+        """Set image to image control.
+
+        Parameters
+        ----------
+        input_path : str
+            Input image path.
+        """
         if input_path is None:
             return
         self.text.value = input_path
